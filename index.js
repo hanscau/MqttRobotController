be66704611db00c7e330bf56f2dcfeefacef39da
Hans Delano <hanscau@hotmail.com>
2020-10-15 14:43:38 +0800

    Serve webpage as webserver

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const app = require("express")();
+const express = require('express');
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const mqtt = require("mqtt");
 
+
 var client = mqtt.connect("mqtt://localhost");
 
 var publish = (topic, payload) => {
-  console.log(`Send ${payload} to ${topic}`);
+  // console.log(`Send ${payload} to ${topic}`);
 };
 
 var broker = "localhost";
@@ -16,12 +18,12 @@ io.on("connection", (socket) => {
   console.log("[Info] user connected");
 
   socket.on("/robot/movement/control", (payload) => {
-    console.log("[Controller] Update robot control", JSON.stringify(payload, null, 2));
+    console.log("[Controller] Update robot control", JSON.stringify(payload));
     publish("/robot/movement/control", JSON.stringify(payload));
   });
 
   socket.on("/robot/camera/control", (payload) => {
-    console.log("[Controller] Update camera control", JSON.stringify(payload, null, 2));
+    console.log("[Controller] Update camera control", JSON.stringify(payload));
     publish("/robot/camera/control", JSON.stringify(payload));
   });
 
@@ -45,6 +47,12 @@ io.on("connection", (socket) => {
   });
 });
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
 http.listen(4000, () => {
-  console.log("[Info] listening on port 4000");
+  console.log("[Info] listening on port http://localhost:4000");
 });
